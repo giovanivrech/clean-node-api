c084dbfac6300f0e3fd95bc4f67fd03318ba5b94
Giovani Vrech <giovanivrech@gmail.com>
2021-03-03 22:13:22 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express-routes-adapter.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest } from '../../presentation/protocols'
+import { Request, Response, RequestHandler } from 'express'
+
+export const adaptRoute = (controller: Controller): RequestHandler => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
