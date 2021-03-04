696319207f2ea76a2eb91cc9191758c0b1cb292f
Giovani Vrech <giovanivrech@gmail.com>
2021-03-03 21:20:16 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://giovanivrech:toor123@clean-code-api.y9wmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050
+}
