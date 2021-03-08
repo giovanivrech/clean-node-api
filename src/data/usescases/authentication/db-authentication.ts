3876c6e4a25aaa9bbac5b67d194cd1013a3a1b24
Giovani Vrech <giovanivrech@gmail.com>
2021-03-08 20:35:58 -0300

    feat: ensure DbAuthentications calls UpdateAcessTokenRepository with correct values

--- src/data/usescases/authentication/db-authentication.ts
+++ src/data/usescases/authentication/db-authentication.ts
@@ -2,20 +2,24 @@ import { Authentication, AuthenticationModel } from '../../../domain/usescases/a
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { UpdateAcessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAcessTokenRepository: UpdateAcessTokenRepository
 
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
-    tokenGenerator: TokenGenerator
+    tokenGenerator: TokenGenerator,
+    updateAcessTokenRepository: UpdateAcessTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAcessTokenRepository = updateAcessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -25,6 +29,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.comparer(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAcessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }
