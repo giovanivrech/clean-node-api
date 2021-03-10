be42677eb2cdbd4b37bd4b17f5ec2ca8e15f5a40
Giovani Vrech <giovanivrech@gmail.com>
2021-03-10 20:04:18 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://giovanivrech:toor123@clean-code-api.y9wmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5h'
 }
