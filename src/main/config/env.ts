64543ac87cda7d5dbe139881a0662c9ddde4ca37
Giovani Vrech <giovanivrech@gmail.com>
2021-03-11 21:19:33 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://giovanivrech:toor123@clean-code-api.y9wmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5h'
 }
