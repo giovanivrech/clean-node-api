48760641b605d53c3b25152c9f7eb95bed272136
Giovani Vrech <giovanivrech@gmail.com>
2021-03-26 21:46:08 -0300

    refactor: change AddSurvey to insert a date also

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -18,7 +18,8 @@ export class AddSurveyController implements Controller {
 
       await this.addSurvey.add({
         question,
-        answers
+        answers,
+        date: new Date()
       })
 
       return noContent()
