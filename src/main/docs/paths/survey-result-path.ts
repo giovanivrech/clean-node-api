7eede347b4fe9b328803e4643ccb740b00d7e805
Giovani Vrech <giovanivrech@gmail.com>
2021-03-31 20:25:50 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -3,6 +3,7 @@ export const surveyResultPath = {
     security: [{
       apiKeyAuth: []
     }],
+    description: 'This route can only be executed by **authenticated users**',
     tags: ['Survey'],
     summary: 'Route to save a answer of the survey',
     requestBody: {
@@ -14,6 +15,43 @@ export const surveyResultPath = {
         }
       }
     },
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey'],
+    summary: 'Route to get a result of the survey',
+    description: 'This route can only be executed by **authenticated users**',
     parameters: [{
       in: 'path',
       name: 'surveyId',
