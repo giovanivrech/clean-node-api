62dccf589e692be7274e6efbb8db62b57068b792
Giovani Vrech <giovanivrech@gmail.com>
2021-03-31 20:17:01 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -86,5 +86,25 @@ describe('Survey Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    test('Should return 200 on load survey result with accessToken', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },
+        {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const accessToken = await makeAccessToken()
+
+      await request(app)
+        .get(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
