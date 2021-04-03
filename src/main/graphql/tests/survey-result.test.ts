b655526d3c37367cbc2b1acce1b942f90508952c
Giovani Vrech <giovanivrech@gmail.com>
2021-04-03 17:57:02 -0300

    test: ensure SurveyResult Query returns AccessDeniedError if no token is provided

--- src/main/graphql/tests/survey-result.test.ts
+++ src/main/graphql/tests/survey-result.test.ts
@@ -111,5 +111,90 @@ describe('SurveyResult GraphQL', () => {
         image: null
       }])
     })
+
+    test('Should return AccessDeniedError if no token is provided', async () => {
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },
+        {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(surveyResultQuery, {
+        variables: {
+          surveyId: surveyRes.ops[0]._id.toString()
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Access denied')
+    })
   })
+
+  describe('SurveyResult Query', () => {
+    const surveyResultQuery = gql`
+        query surveyResult ($surveyId: String!) {
+          surveyResult (surveyId: $surveyId) {
+              question
+              answers {
+                image
+                answer
+                count
+                percent
+                isCurrentAccountAnswer
+              }
+              date
+            }
+        }
+    `
+    test('Should return surveyResult', async () => {
+      const accessToken = await makeAccessToken()
+      const now = new Date()
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },
+        {
+          answer: 'Answer 2'
+        }],
+        date: now
+      })
+
+      const { query } = createTestClient({
+        apolloServer,
+        extendMockRequest: {
+          headers: {
+            'x-access-token': accessToken
+          }
+        }
+      })
+      const res: any = await query(surveyResultQuery, {
+        variables: {
+          surveyId: surveyRes.ops[0]._id.toString()
+        }
+      })
+      expect(res.data.surveyResult.question).toBe('Question')
+      expect(res.data.surveyResult.date).toBe(now.toISOString())
+      expect(res.data.surveyResult.answers).toEqual([{
+        answer: 'Answer 1',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false,
+        image: 'http://image-name.com'
+      },
+      {
+        answer: 'Answer 2',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false,
+        image: null
+      }])
+    })
 })
