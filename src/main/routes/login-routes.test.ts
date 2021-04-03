cf8c53a4aa0455906a4b42f11290081b1deae809
Giovani Vrech <giovanivrech@gmail.com>
2021-04-03 15:23:45 -0300

    test: ensure Login Query retruns an account on valid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,8 @@
-import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
 
 let accountCollection: Collection
 
